32e29af470a50df8e98f77a6134dedfb75967e88
monoliterato <monoliterato@gmail.com>
2020-02-28 21:36:02 -0400

    datos de geolocalizacion

--- src/app/camera/pics/pics.component.ts
+++ src/app/camera/pics/pics.component.ts
@@ -3,20 +3,42 @@ var fs= require("file-system");
 let imagepicker= require("nativescript-imagepicker");
 
 import { takePicture } from 'nativescript-camera';
+
+
 import {ImageSource, fromFile, fromResource, fromBase64} from "tns-core-modules/image-source";
 import {Folder, path, knownFolders} from "tns-core-modules/file-system";
 
 
+//galerias de la geolocalizacion para obtener datos de el lugar donde se realiza la captura de la forografía
+
+import * as geolocation from "nativescript-geolocation";
+import { Accuracy } from "tns-core-modules/ui/enums"; // used to describe at what accuracy the location should be get
+
+
+//al demonio wa usar los fancy alerts 
+import { TNSFancyAlert, TNSFancyAlertButton } from "nativescript-fancyalert";
+
+
+//bien ahora deberemos usar las alertas pero con esta galeria haber si funciona
+import * as dialogs from "tns-core-modules/ui/dialogs";
+
 @Component({
   selector: 'ns-pics',
   templateUrl: './pics.component.html',
-  styleUrls: ['./pics.component.css']
+  styleUrls: ['./pics.component.css'],
+
 })
 export class PicsComponent implements OnInit {
   public saveImage;
   constructor() { }
 
   ngOnInit(): void {
+
+    
+    
+
+
+
   }
 
 
@@ -35,6 +57,38 @@ source.fromAsset(img).then((source)=>{
 } );
 
   }));
+}
+
+
+
+alerta(){
+
+
+ // dialogs.alert("Your message").then(() => {
+   // console.log("Dialog closed!");
+//});
+
+  geolocation.enableLocationRequest()
+  .then(()=>  {geolocation.getCurrentLocation({desiredAccuracy:Accuracy.high})
+  .then((location)=>{dialogs.alert(JSON.stringify(location));
+  })
+  })
+
+}
+
+
+elemento(){
+
+
+
+  TNSFancyAlert.showSuccess(
+    "Success!",
+    "This uses a custom width of 300.",
+    `Oh that's nice.`,
+    0,
+    300
+  );
+  
 }
 
 }
